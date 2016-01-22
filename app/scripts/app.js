63d31470255aa86d0b6f0dfefd4cd6d4d44f09d9
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2016-01-22 13:33:59 +0530

    sparta frontend app; init

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name spartaFrontendApp
+ * @description
+ * # spartaFrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('spartaFrontendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/articles', {
+        templateUrl: 'views/articles.html',
+        controller: 'ArticlesCtrl',
+        controllerAs: 'articles'
+      })
+      .otherwise({
+        redirectTo: '/articles'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name spartaFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the spartaFrontendApp
+ */
+angular.module('spartaFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
