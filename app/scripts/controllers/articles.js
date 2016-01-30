a93a23a23411714950aacf1a8927e1537e1bf803
Deepankar Bajpeyi <dbajpeyi@gmail.com>
2016-01-30 14:38:11 +0530

    check authenticated and remove article on click

--- app/scripts/controllers/articles.js
+++ app/scripts/controllers/articles.js
@@ -8,8 +8,32 @@
  * Controller of the spartaFrontendApp
  */
 angular.module('spartaFrontendApp')
-  .controller('ArticlesCtrl', function ($scope, Articles) {
+  .controller('ArticlesCtrl', function ($scope, $filter, Articles, ArticleAction, $auth, $location) {
+
+  	$scope.isAuthenticated = function () {
+  		 return $auth.isAuthenticated();  
+  	};
+
+  	$scope.gologin = function  () {
+  		$location.path('/login');
+  	};
+
+
+  	$scope.articleAction = function(article, is_liked) {
+  		console.log(is_liked, article.ext_id);
+  		 ArticleAction.save({
+  		 	'is_liked' : is_liked,
+  		 	'ext_id' : article.ext_id
+  		 }, function(data){
+  		 	angular.forEach($scope.articles, function(item){
+  		 		$scope.articles = $filter('filter')($scope.articles, {'ext_id' : '!' + data.article.ext_id})
+  		 	});
+  		 });
+  	};
+
   	Articles.get({}, function(data){
   		$scope.articles = data;
+  		console.log(data);
   	});
+
   });
